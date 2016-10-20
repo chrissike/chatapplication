ecc5cc264d0bb18570f95f16ef42ba719a1c80b8
CodeMax <max.spelsberg@gmail.com>
2016-10-20 16:51:55 +0200

    added logging to processChatPDU

--- chatApplicationServer/src/main/java/edu/hm/dako/chat/server/process/ProcessChatPDU.java
+++ chatApplicationServer/src/main/java/edu/hm/dako/chat/server/process/ProcessChatPDU.java
@@ -1,13 +1,17 @@
 package edu.hm.dako.chat.server.process;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import edu.hm.dako.chat.common.ChatPDU;
+import edu.hm.dako.chat.server.SimpleChatServerImpl;
 
 public class ProcessChatPDU {
 
+	private static Log log = LogFactory.getLog(SimpleChatServerImpl.class);
+	
 	public void process(ChatPDU pdu) {
-		
-		//TODO 
-		
+		log.info("JMS-Nachricht ist angekommen: " + pdu.toString());
 	}
 	
 }
