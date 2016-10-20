bea8860e0f0c9b3fed58ce633900185df8c8d536
CodeMax <max.spelsberg@gmail.com>
2016-10-20 17:43:53 +0200

    added logging to processChatPDU

--- chatApplicationServer/src/main/java/edu/hm/dako/chat/server/jms/MessageListenerImpl.java
+++ chatApplicationServer/src/main/java/edu/hm/dako/chat/server/jms/MessageListenerImpl.java
@@ -3,20 +3,32 @@ package edu.hm.dako.chat.server.jms;
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
+import javax.ejb.MessageDriven;
+import javax.inject.Inject;
+import javax.ejb.ActivationConfigProperty;
 
 import edu.hm.dako.chat.common.ChatPDU;
+import edu.hm.dako.chat.server.process.ProcessChatPDU;
 
+@MessageDriven( // Message-driven bean (MDB)
+activationConfig = {
+	@ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
+	@ActivationConfigProperty(propertyName = "destination", propertyValue = "/queue/JarfillerQueue" // destination's JNDI name											// name
+) })
 public class MessageListenerImpl implements MessageListener {
 
-	@Override
+	@Inject
+	ProcessChatPDU processChatPDU;
+	
 	public void onMessage(Message message) {
-		// TODO Auto-generated method stub
+		
 		try {
 			ChatPDU chatPDU = message.getBody(ChatPDU.class);
+			processChatPDU.process(chatPDU);
 		} catch (JMSException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
 	}
-
 }
