ba9f36706410488f89b601654cf691d8a6de27bb
CodeMax <max.spelsberg@gmail.com>
2016-10-20 18:40:44 +0200

    log class

--- chatApplicationServer/src/main/java/edu/hm/dako/chat/server/process/ProcessChatPDU.java
+++ chatApplicationServer/src/main/java/edu/hm/dako/chat/server/process/ProcessChatPDU.java
@@ -4,11 +4,10 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import edu.hm.dako.chat.common.ChatPDU;
-import edu.hm.dako.chat.server.SimpleChatServerImpl;
 
 public class ProcessChatPDU {
 
-	private static Log log = LogFactory.getLog(SimpleChatServerImpl.class);
+	private static Log log = LogFactory.getLog(ProcessChatPDU.class);
 	
 	public void process(ChatPDU pdu) {
 		log.info("JMS-Nachricht ist angekommen: " + pdu.toString());
