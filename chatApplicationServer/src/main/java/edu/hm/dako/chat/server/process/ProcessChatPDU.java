9f28cfc5d397b4f22540a484ae3a1d507ab187ea
CodeMax <max.spelsberg@gmail.com>
2016-10-20 16:28:51 +0200

    added chatpdu process

---
+++ chatApplicationServer/src/main/java/edu/hm/dako/chat/server/process/ProcessChatPDU.java
@@ -0,0 +1,13 @@
+package edu.hm.dako.chat.server.process;
+
+import edu.hm.dako.chat.common.ChatPDU;
+
+public class ProcessChatPDU {
+
+	public void process(ChatPDU pdu) {
+		
+		//TODO 
+		
+	}
+	
+}
