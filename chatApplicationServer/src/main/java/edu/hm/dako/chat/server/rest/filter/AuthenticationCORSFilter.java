0dfbc59de6cfb00b2fb13b4d09319fa160c95fcb
CodeMax <max.spelsberg@gmail.com>
2016-11-25 17:20:28 +0100

    CORS filter auf jersey umgestellt

--- chatApplicationServer/src/main/java/edu/hm/dako/chat/server/rest/filter/AuthenticationCORSFilter.java
+++ chatApplicationServer/src/main/java/edu/hm/dako/chat/server/rest/filter/AuthenticationCORSFilter.java
@@ -2,53 +2,32 @@ package edu.hm.dako.chat.server.rest.filter;
 
 import java.io.IOException;
 
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
-public class AuthenticationCORSFilter implements Filter {
+/**
+ * Filter für Anfragen von Javascript. (siehe CORS)
+ */
+@Provider
+public class AuthenticationCORSFilter implements ContainerResponseFilter {
 
-	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) {
-		HttpServletResponse response = (HttpServletResponse) res;
-		HttpServletRequest request = (HttpServletRequest) req;
-
-		if (request.getServletPath().contains("root") || request.getRequestURI().contains("root/user")) {
-
-			response.setHeader("Access-Control-Allow-Origin", "*");
-			response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
-			response.setHeader("Access-Control-Allow-Headers",
-					"Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-			response.setHeader("Access-Control-Max-Age", "3600");
-			if (request.getMethod() != "OPTIONS") {
-				try {
-					chain.doFilter(req, res);
-				} catch (IOException e) {
-					e.printStackTrace();
-				} catch (ServletException e) {
-					e.printStackTrace();
-				}
-			} else {
-			}
-
-		}else{
-			try {
-				chain.doFilter(req, res);
-			} catch (IOException | ServletException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-
-	public void init(FilterConfig filterConfig) throws ServletException {
-	}
-
-	public void destroy() {
-	}
+	private static final Log log = LogFactory.getLog(AuthenticationCORSFilter.class);
+		
+		@Override
+	    public void filter(ContainerRequestContext request,
+	            ContainerResponseContext response) throws IOException {
+			log.info("CORS-filter aufgerufen");
+	        response.getHeaders().add("Access-Control-Allow-Origin", "*");
+	        response.getHeaders().add("Access-Control-Allow-Headers",
+	                "origin, content-type, accept, authorization");
+	        response.getHeaders().add("Access-Control-Allow-Credentials", "true");
+	        response.getHeaders().add("Access-Control-Allow-Methods",
+	                "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+	    }
 
 }
