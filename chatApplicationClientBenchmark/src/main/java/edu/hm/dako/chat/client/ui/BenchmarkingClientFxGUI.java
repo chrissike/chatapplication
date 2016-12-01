8cb3a3e4d5b574a1c9900955826ed800323319f8
david <david.sautter@web.de>
2016-12-01 20:05:51 +0100

    minor improvements

--- chatApplicationClientBenchmark/src/main/java/edu/hm/dako/chat/client/ui/BenchmarkingClientFxGUI.java
+++ chatApplicationClientBenchmark/src/main/java/edu/hm/dako/chat/client/ui/BenchmarkingClientFxGUI.java
@@ -1,20 +1,13 @@
 package edu.hm.dako.chat.client.ui;
 
-import java.util.stream.Collectors;
-
-import javax.naming.NamingException;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
 import edu.hm.dako.chat.client.benchmarking.TopicSubscriber;
-import edu.hm.dako.chat.jms.connect.JmsChatContext;
-import edu.hm.dako.chat.jms.connect.JmsConsumer;
 import edu.hm.dako.chat.client.data.ClientModel;
 import edu.hm.dako.chat.client.data.GroupedResultTableModel;
 import edu.hm.dako.chat.client.data.ResultTableModel;
 import edu.hm.dako.chat.client.data.SystemStatus;
 import edu.hm.dako.chat.client.data.util.SystemResourceCalculator;
+import edu.hm.dako.chat.jms.connect.JmsChatContext;
+import edu.hm.dako.chat.jms.connect.JmsConsumer;
 import edu.hm.dako.chat.model.BenchmarkPDU;
 import javafx.application.Application;
 import javafx.application.Platform;
@@ -22,6 +15,11 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import javax.naming.NamingException;
+import java.util.stream.Collectors;
 
 /**
  * Chat-GUI
@@ -129,7 +127,7 @@ public class BenchmarkingClientFxGUI extends Application {
 			getSysStatus().addFreeServerMemoryList(pdu.getFreeMemory().intValue());
 
 			// Table
-			ResultTableModel resulttable = new ResultTableModel(pdu.getMessageNr().toString(), "1",
+			ResultTableModel resultTableRow = new ResultTableModel(pdu.getMessageNr().toString(), "1",
 					String.valueOf(rttMs), String.valueOf(rttServerMs), pdu.getFreeMemory().toString(),
 					pdu.getAvgCPUUsage().toString());
 
@@ -140,7 +138,7 @@ public class BenchmarkingClientFxGUI extends Application {
 
 			log.info(">>>> GroupedResultTable: " + groupedResulttable.toString());
 
-			getModel().addToResultList(resulttable);
+			getModel().addToResultList(resultTableRow);
 
 			// calculate KPIs
 			getModel().calculateKPIs();
