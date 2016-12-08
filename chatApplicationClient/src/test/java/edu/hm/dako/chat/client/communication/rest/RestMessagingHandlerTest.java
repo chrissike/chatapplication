3b4ac17933d09d0238c57aca0a4bcc5747a46249
david <david.sautter@web.de>
2016-12-08 15:59:39 +0100

    Rückgängig: IP-Adresse von Wildfly-VM eingefügt

--- chatApplicationClient/src/test/java/edu/hm/dako/chat/client/communication/rest/RestMessagingHandlerTest.java
+++ chatApplicationClient/src/test/java/edu/hm/dako/chat/client/communication/rest/RestMessagingHandlerTest.java
@@ -1,15 +1,14 @@
 package edu.hm.dako.chat.client.communication.rest;
 
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
-
 import edu.hm.dako.chat.rest.MessagingHandler;
 import edu.hm.dako.chat.rest.MessagingHandlerImpl;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
 
 public class RestMessagingHandlerTest {
 
-	private static final String ADDRESS = "10.28.56.110";
+	private static final String ADDRESS = "127.0.0.1";
 	private static final Integer PORT = 8089;
 
 
